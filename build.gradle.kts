938f7379b7e9a649557fb55a9beb35f31039ba74
Irene Rivero <irene.rivero.casal@gmail.com>
2026-03-18 11:06:16 +0100

    chore: initial project setup with Ktor and Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,29 @@
 plugins {
     kotlin("jvm") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
+    application
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.executor"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-core:2.3.7")
+    implementation("io.ktor:ktor-server-netty:2.3.7")
+    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("ch.qos.logback:logback-classic:1.4.11")
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
+application {
+    mainClass.set("com.executor.ApplicationKt")
 }
+
 kotlin {
     jvmToolchain(23)
 }
